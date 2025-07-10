2fd1593eab2fda0e317c70de643eb63365a3fb06
SHAKTHI-2003 <Shakthi.M@cognizant.com>
2025-07-10 18:22:56 +0530

    This is Good

--- src/main/java/hackathon/Hackathon.java
+++ src/main/java/hackathon/Hackathon.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 public class Hackathon
 {
